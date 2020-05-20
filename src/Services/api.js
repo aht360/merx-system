7997bccec671432414ba34d83a8559f580c1072a
Jhonatan <jhonatankennedy@live.com>
2020-05-19 21:48:33 -0300

    front

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
  
 const api  = axios.create({
-    baseURL:'https://backendmerx.herokuapp.com/',
+    baseURL:'https://merxbackend.herokuapp.com/',
 })
 export default api;
