5f143d647e6defafbc8c714735e2eb75fc857b53
Augusto <aht@cin.ufpe.br>
2020-06-01 20:13:59 -0300

    Adding dados ccee for pesquisa ml

---
+++ src/Components/DadosCCEE/index.js
@@ -0,0 +1,70 @@
+import React, { Component }  from 'react';
+
+import './style.css';
+
+
+
+
+
+
+class App extends Component{
+
+    constructor(props){
+        super();
+        this.state = {
+            data: props.data
+        }
+    }
+
+    render(){
+
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "center", color: "white"}}>
+            <div style={{display: "flex", flexDirection: "column", marginTop: "50px", backgroundColor: "#2F80ED", width: "1200px", padding: "30px", borderRadius: "10px"}}>
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                    <p style={{fontSize: "30px", fontWeight: "bold"}}>Dados CCEE</p>
+                </div>
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                    <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                        <p style={{fontWeight: "bold", fontSize: "22px"}}>Código do agente: </p>
+                        <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].CodAgente}</p>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                        <p style={{fontWeight: "bold", fontSize: "22px"}}>Nome: </p>
+                        <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].SiglaAgente}</p>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                        <p style={{fontWeight: "bold", fontSize: "22px"}}>Categoria: </p>
+                        <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].CategoriaAgente}</p>
+                    </div>
+                </div>
+                
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                    <p style={{width: "200px"}}>Código Perfil</p>
+                    <p style={{width: "200px"}}>Sigla</p>
+                    <p style={{width: "200px"}}>Classe</p>
+                    <p style={{width: "200px"}}>Status</p>
+                    <p style={{width: "200px"}}>Submercado</p>
+                    <p style={{width: "200px"}}>Perfil Varejista</p>
+                </div>
+                {
+                this.state.data.map(item => (
+                    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                        <p style={{width: "200px"}}>{item.CodPerfilAgente}</p>
+                        <p style={{width: "200px"}}>{item.SiglaPerfilAgente}</p>
+                        <p style={{width: "200px"}}>{item.ClassePerfilAgente}</p>
+                        <p style={{width: "200px"}}>{item.StatusPerfilAgente}</p>
+                        <p style={{width: "200px"}}>{item.SubmercadoPerfilAgente}</p>
+                        <p style={{width: "200px"}}>{item.PerfilVarej}</p>
+                    </div>
+                ))
+                    
+                }
+                
+            </div>
+        </div>
+
+    }
+}
+
+
+export default App;
--- src/pages/Pesquisa/index.js
+++ src/pages/Pesquisa/index.js
@@ -14,14 +14,15 @@ class App extends Component{
 
     state = {
         SiglaAgente: "",
-        data: {},
+        data: [],
         show: false
     };
 
 
     handleSearch = async e => {
         e.preventDefault();
-        const { SiglaAgente } = this.state;
+        var { SiglaAgente } = this.state;
+        SiglaAgente = SiglaAgente.toUpperCase();
         
         if (!SiglaAgente) {
             alert("Preencha todos os dados para fazer a pesquisa");
@@ -30,17 +31,24 @@ class App extends Component{
             try {
                 
                 const response = await api.post("/DadosCadastrais", { SiglaAgente });
-
-                console.log(response);
-                alert('pesquisa feita!');
+                
+                console.log(response.data.length)
+                
                 this.setState({data: response.data});
-                this.setState({show: true});
-                console.log(this.state.data.NomeAgente);
+
+                if(response.data.length !== 0){
+                    this.setState({show: true});
+
+                }
+                else{
+                    this.setState({show: false});
+                    alert('Não existe empresa com essa sigla no nosso banco de dados')
+                }
 
             } catch (err) {
-                
                 console.log(err);
                 alert("Não existe empresa com essa sigla no nosso banco de dados");
+                this.setState({show: false});
                 
             }
         }
@@ -51,70 +59,81 @@ class App extends Component{
 
 
         return(
-            <div className="container-data-master" style={{textAlign: "center"}}>
+            <div style={{textAlign: "center"}}>
                 <NavBar changeColor="false"/>
-                <h1>Pesquisa Mercado Livre</h1>
-                <Form onSubmit={this.handleSearch}>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Control
-                            className="text-center center-input"
-                            size="lg"
-                            type="text"
-                            placeholder="Insira a sigla da empresa"
-                            style={{width: "268px"}}
-                            onChange={e => this.setState({ SiglaAgente: e.target.value })}
-                        />
-                    </Form.Group>
-    
-                    <button className="btn-pesquisaml" type="submit">Pesquisar</button>
-                
-                </Form>
+                <div className="container-pesquisa-master">
+                    <h1>Pesquisa Mercado Livre</h1>
+                    <Form onSubmit={this.handleSearch}>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Control
+                                className="text-center center-input"
+                                size="lg"
+                                type="text"
+                                placeholder="Insira a sigla da empresa"
+                                style={{width: "268px"}}
+                                onChange={e => this.setState({ SiglaAgente: e.target.value })}
+                            />
+                        </Form.Group>
+        
+                        <button className="btn-pesquisaml" type="submit">Pesquisar</button>
+                    
+                    </Form>
 
-                {
-                this.state.show === true &&
-                <div className="data-ml">
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Cód. Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.CodAgente}</p>
+
+                    {
+
+                    this.state.show === true &&
+                    this.state.data[0] !== undefined &&
+                    <div style={{display: "flex", flexDirection: "row", justifyContent: "center", color: "black"}}>
+                        <div style={{display: "flex", flexDirection: "column", marginTop: "50px", backgroundColor: "white", width: "1200px", padding: "30px", borderRadius: "10px", boxShadow: "0px 6px 18px rgba(0, 0, 0, 0.06)"}}>
+                            <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                                <p style={{fontSize: "30px", fontWeight: "bold"}}>Dados CCEE</p>
+                            </div>
+                            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around", borderBottom: "solid", borderWidth: '1px'}}>
+                                <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                                    <p style={{fontWeight: "bold", fontSize: "22px"}}>Código do agente: </p>
+                                    <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].CodAgente}</p>
+                                </div>
+                                <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                                    <p style={{fontWeight: "bold", fontSize: "22px"}}>Nome: </p>
+                                    <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].SiglaAgente}</p>
+                                </div>
+                                <div style={{display: "flex", flexDirection: "row", alignItems: "baseline"}}>
+                                    <p style={{fontWeight: "bold", fontSize: "22px"}}>Categoria: </p>
+                                    <p style={{fontSize: "18px", marginLeft: "10px"}}>{this.state.data[0].CategoriaAgente}</p>
+                                </div>
+                            </div>
+                            
+                            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around", borderBottom: "solid", borderWidth: '1px'}}>
+                                <p style={{width: "200px"}}>Código Perfil</p>
+                                <p style={{width: "200px"}}>Sigla</p>
+                                <p style={{width: "200px"}}>Classe</p>
+                                <p style={{width: "200px"}}>Status</p>
+                                <p style={{width: "200px"}}>Submercado</p>
+                                <p style={{width: "200px"}}>Perfil Varejista</p>
+                            </div>
+                            {
+                            this.state.data.map(item => (
+                                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around", borderBottom: "solid", borderWidth: '1px'}}>
+                                    <p style={{width: "200px"}}>{item.CodPerfilAgente}</p>
+                                    <p style={{width: "200px"}}>{item.SiglaPerfilAgente}</p>
+                                    <p style={{width: "200px"}}>{item.ClassePerfilAgente}</p>
+                                    <p style={{width: "200px"}}>{item.StatusPerfilAgente}</p>
+                                    <p style={{width: "200px"}}>{item.SubmercadoPerfilAgente}</p>
+                                    <p style={{width: "200px"}}>{item.PerfilVarej}</p>
+                                </div>
+                            ))
+                                
+                            }
+                            
+                        </div>
                     </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Sigla do Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.SiglaAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Nome Empresarial do Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.NomeAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">CNPJ: </p>
-                        <p className="data-ml-row-content">{'0' + this.state.data.CnpjAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Cód Perfil de Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.CodPerfilAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Classe do Perfil de Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.ClassePerfilAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Status do Perfil de Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.StatusPerfilAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Categoria do Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.CategoriaAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Submercado do Perfil de Agente: </p>
-                        <p className="data-ml-row-content">{this.state.data.SubmercadoPerfilAgente}</p>
-                    </div>
-                    <div className="data-ml-row">
-                        <p className="data-ml-row-title">Perfil Varejista: </p>
-                        <p className="data-ml-row-content">{this.state.data.PerfilVarej}</p>
-                    </div>
+
+                    
+
+                    }
+
                 </div>
-                }
 
                 <Footer />
             </div>
