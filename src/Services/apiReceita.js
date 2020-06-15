6651087d73cac3cc18313b5021da732acb6e8bea
Augusto <aht@cin.ufpe.br>
2020-06-15 09:07:44 -0300

    fix

--- src/Services/apiReceita.js
+++ src/Services/apiReceita.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiReceita = axios.create({
-    baseURL:'https://cors-anywhere.herokuapp.com/https://www.receitaws.com.br/v1/cnpj/'
+    baseURL:'https://www.receitaws.com.br/v1/cnpj/'
     
 });
 
